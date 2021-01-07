238cafc7af770dd3fca1d85de2166311a62020fd
Gursewak <gs2280431@gmail.com>
2021-01-07 10:39:53 +0530

    adding sending messenges options

--- source.py
+++ source.py
@@ -1,13 +1,12 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
-import re
-import csv
+import os
 # beautiful soup object and response from website
 response = requests.get(r'https://www.freejobalert.com/punjab-government-jobs/')
 soup = BeautifulSoup(response.content, 'lxml')
 
 import smtplib, ssl
-'''
+
 port = 465  # For SSL
 password = input("Type your password and press enter: ")
 
@@ -17,16 +16,17 @@ context = ssl.create_default_context()
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login("my@gmail.com", password)
     # TODO: Send email here
-'''
+
 job  = []
 
 get_detail_links = [] # link to post
 
-with open(job_file.csv, 'w') as csvfile:
-    #getting job details
-    for text in soup.find_all('tr', attrs={"style":"border: 1px solid #000000;"}):
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(text.get_text())
-    # TODO: getting link
-    for link in soup.find_all('strong'):
-        print(link.find('a').attrs['href'])
+#getting job details
+for text in soup.find_all('tr', attrs={"style":"border: 1px solid #000000;"}):
+    text.get_text()
+# TODO: getting link
+for link in soup.find_all('strong'):
+    link.find('a').attrs['href']
+
+print(os.environ.get("gmail"))
+print(os.environ.get("password"))
